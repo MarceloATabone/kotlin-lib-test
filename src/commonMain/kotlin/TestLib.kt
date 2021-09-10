27ec9f38407680593febab7ac46a552ed604feda
Marcelo A Tabone <marceloatabone@hotmail.com>
2021-09-10 16:35:18 -0300

    Created class for test

---
+++ src/commonMain/kotlin/TestLib.kt
@@ -0,0 +1,12 @@
+class TestLib {
+
+    val zero : Int = 0
+
+    fun returnZero(): Int{
+        return zero
+    }
+
+    fun returnString(str:String): String{
+        return str
+    }
+}
--- src/commonTest/kotlin/main.kt
+++ src/commonTest/kotlin/main.kt
@@ -1,6 +1,6 @@
-class main {
 
-    fun main(args:Array<String>) {
-        println("hello")
-    }
+fun main(args:Array<String>) {
+    println("hello")
+    var testLib = TestLib()
+    println("testLib: ${testLib.returnString("HEY!!!")}")
 }
